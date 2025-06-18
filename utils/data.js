119a46cff34dcafafb650ada8699b7a5cddf5351
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2025-06-18 17:50:22 -0500

    feat: Cambios en los textos, estilos y datos

--- utils/data.js
+++ utils/data.js
@@ -1,18 +1,19 @@
 export default {
- recolect :{
+ recolect : {
     content: { 
         title: "Recolectar los colinos",
         icon: "public/icons/shovel.svg",
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem"     
+        information: "Se seleccionan colinos sanos provenientes de la planta madre (como coronas o hijuelos), se les retiran las hojas secas y se dejan reposar entre 3 y 7 días a la sombra para que cicatricen y evitar que se pudran al sembrarlosSe seleccionan colinos sanos provenientes de la planta madre (como coronas o hijuelos), se les retiran las hojas secas y se dejan reposar entre 3 y 7 días a la sombra para que cicatricen y evitar que se pudran al sembrarlos"
+             
     } 
- },
+ }, 
  repair: {
     content: { 
         title: "Preparar el terreno",
         icon: "public/icons/pick.svg",
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: "El suelo se limpia de maleza y se labra para aflojarlo y mejorar el drenaje. Si es necesario, se hacen camellones o surcos. También se puede aplicar abono orgánico para mejorar la fertilidad del terreno."
     }
  },
  sow: {
@@ -20,7 +21,7 @@ export default {
         title: "Sembrar el colino",
         icon: "public/icons/bucket-droplet.svg",
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: "Se hacen huecos de 10 a 15 cm, se colocan los colinos en posición vertical y se cubren hasta la base de las hojas. Se recomienda una distancia de 30 a 50 cm entre plantas y se riega ligeramente después de sembrar." 
     }
  },
  fertilize: {
@@ -28,7 +29,7 @@ export default {
         title: "Abonar",
         icon: "public/icons/leaf.svg",
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: "Se aplica fertilizante al cultivo para proporcionarle los nutrientes necesarios que favorecen su crecimiento y producción." 
     }
  },
  cleanUp: {
@@ -36,7 +37,8 @@ export default {
         title: "Desyerbar",
         icon: "public/icons/clean.svg", 
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: "Se eliminan las malezas del terreno, ya que compiten con el cultivo por agua, luz y nutrientes, afectando su desarrollo."
+         
     }
  },
  fumigate: {
@@ -44,7 +46,8 @@ export default {
         title: "Fumigar",
         icon: "public/icons/spray.svg",
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: "   Se aplican productos para controlar plagas y enfermedades que puedan dañar la planta o el fruto, garantizando una cosecha sana."
+
     }
  },
  gather: {
@@ -52,7 +55,7 @@ export default {
         title: "Recolectar",
         icon: "public/icons/basket.svg", 
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: " Cuando el fruto alcanza su madurez, se realiza la cosecha de forma cuidadosa para evitar pérdidas o daños al producto " 
     }
  },
 
@@ -61,7 +64,7 @@ export default {
         title: "Transportar",
         icon: "public/icons/truck.svg", 
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+        information: " Finalmente, se trasladan los productos desde la finca hasta los puntos de venta, procurando conservar su frescura y calidad." 
     }
  },
 
@@ -70,7 +73,9 @@ export default {
         title: "Distribuir",
         icon: "public/icons/cash.svg", 
         imageUrl: "NaN", 
-        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
-    }
- } 
+        information: "Los productos se clasifican por calidad y tamaño, y se organizan para su venta o entrega a compradores locales o mayoristas." 
+
+    } 
+}
+
 }
