893e958e4bed447b5f8d765087bc673d3c2afe1d
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2025-06-18 12:52:26 -0500

    feat: Agregue funcionalidad de popUp
    Se realizo la programacion necesaria para la creacion de la estructura y su funcionalidad

---
+++ utils/data.js
@@ -0,0 +1,76 @@
+export default {
+ recolect :{
+    content: { 
+        title: "Recolectar los colinos",
+        icon: "public/icons/shovel.svg",
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem"     
+    } 
+ },
+ repair: {
+    content: { 
+        title: "Preparar el terreno",
+        icon: "public/icons/pick.svg",
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+ sow: {
+    content: { 
+        title: "Sembrar el colino",
+        icon: "public/icons/bucket-droplet.svg",
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+ fertilize: {
+    content: { 
+        title: "Abonar",
+        icon: "public/icons/leaf.svg",
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+ cleanUp: {
+    content: { 
+        title: "Desyerbar",
+        icon: "public/icons/clean.svg", 
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+ fumigate: {
+    content: { 
+        title: "Fumigar",
+        icon: "public/icons/spray.svg",
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+ gather: {
+    content: { 
+        title: "Recolectar",
+        icon: "public/icons/basket.svg", 
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+
+ transport: {
+    content: { 
+        title: "Transportar",
+        icon: "public/icons/truck.svg", 
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ },
+
+ distribute: {
+    content: { 
+        title: "Distribuir",
+        icon: "public/icons/cash.svg", 
+        imageUrl: "NaN", 
+        information: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus obcaecati tenetur, nisi totam officiis aliquam blanditiis quia similique dolorem" 
+    }
+ } 
+}
--- utils/process.js
+++ utils/process.js
@@ -1,1 +1,94 @@
-const sectionProcess = document.querySelector("")
+import data from "./data.js";
+
+const sectionProcess = document.querySelector("#process");
+const closeIcon = "public/icons/close.svg"; 
+
+const openModal = (buttonOpen, modal) => { 
+    buttonOpen.addEventListener("click", () => {
+        modal.style.display = "flex"; 
+    })
+}
+// refactor this code!!! 
+const closeModal = (buttonClose, modal) => { 
+    let children = modal.firstChild; 
+
+    buttonClose.addEventListener("click", () => { 
+        children.style.animation = "closeModal 500ms"; 
+        setTimeout(() => {
+            modal.style.display = "none"; 
+            children.style.animation = ""; 
+        }, 300); 
+    });
+
+    window.addEventListener("click", (e) => { 
+        if(e.target == modal) { 
+            children.style.animation = "closeModal 500ms"; 
+            setTimeout(() => {
+                modal.style.display = "none"; 
+                children.style.animation = ""; 
+            }, 300); 
+        }
+    })
+}
+
+const generateModal = (key, data) => { 
+    let modal = document.createElement("div"); 
+    let modalContent = document.createElement("div"); 
+    let buttonClose = document.createElement("span");
+
+    modal.classList.add(`modal`); 
+    modalContent.classList.add(`modal-content`); 
+    modal.style.display = "none"; 
+
+    // add modal content 
+    buttonClose.classList.add("btn-close");
+    buttonClose.innerHTML = `<img src="${closeIcon}" alt="close modal icon"/>`;
+    
+    modalContent.innerHTML = `
+        <div>
+            <h3>${data.content.title}</h3>
+            <p>${data.content.information}</p>
+        </div>
+    `
+
+    modalContent.appendChild(buttonClose); 
+    modal.appendChild(modalContent); 
+    sectionProcess.appendChild(modal); 
+    
+    // generate buttons 
+    generateButtons(key, data, modal); 
+
+    // close this modal 
+    closeModal(buttonClose, modal); 
+}
+
+const generateButtons = (key, data, modal) => { 
+    let containerButton = document.createElement("div") 
+    let span = document.createElement("span")
+    let button = document.createElement("button")
+    
+    button.classList.add("btn-process"); 
+    button.id = `button-${key}` 
+    containerButton.classList.add(`container-button`)
+    span.classList.add(`title-${key}`)
+
+    button.innerHTML = `<img src="${data.content.icon}" alt="image-${data.content.title}">`;
+    span.textContent = data.content.title;
+    
+    containerButton.appendChild(button); 
+    containerButton.appendChild(span); 
+    sectionProcess.appendChild(containerButton); 
+    
+    // add event to open modal
+    openModal(button, modal)
+}
+
+const PrintProcess = () => {
+    for (const key in data) {
+        generateModal(key, data[key]); // generate modals
+    } 
+}
+
+PrintProcess();
+
+
